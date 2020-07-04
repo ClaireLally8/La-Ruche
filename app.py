542164cce36a19deda5d262257263a055c722fa3
Claire Lally <clairelally08@gmail.com>
2020-07-04 14:27:49 +0000

    add info to run

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def register():
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
